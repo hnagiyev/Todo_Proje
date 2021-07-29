180976fdb2300f1490b3066ef91693b95bcc5260
hnagiyev <huseyn.naghiyev@gmail.com>
2021-07-29 21:33:59 +0400

    first commit

---
+++ todo_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Todos
+
+# Create your views here.
+def index(request):
+    name='Huseyn'
+    todo_list = Todos.objects.all()
+    return render(request, 'todo_app/index.html',{'todo_list':todo_list})
+
+def about(request):
+    return render(request, 'todo_app/about.html')
+
+def create(request):
+    return render(request, 'todo_app/create.html')
